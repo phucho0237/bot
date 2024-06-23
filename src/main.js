2834a759692ca913eb26acb34f15509120a5cef5
Nhu Phuc <phucho0237@gmail.com>
2024-06-23 13:12:09 +0700

    remove unused line (commented)

--- src/main.js
+++ src/main.js
@@ -23,7 +23,6 @@ const client = new Client({
 
 const player = new Player(client);
 player.extractors.loadDefault();
-// player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
 
 client.commands = new Collection();
 
